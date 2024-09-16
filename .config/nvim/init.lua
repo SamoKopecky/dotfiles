1407724b363fd450b10c23a08eb5ef2f4719c6b4
SamoKopecky <sano.kopecky@gmail.com>
2024-09-16 22:05:50 +0200

    feat(tmux): Kill window and pane with no confirm
    Signed-off-by: SamoKopecky <sano.kopecky@gmail.com>

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -754,6 +754,7 @@ require('lazy').setup({
             },
           },
         },
+        -- pyright = {},
         rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
