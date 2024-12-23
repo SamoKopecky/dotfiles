7fbb3740b6f85738d7eab2a12bd30fa1ed7e2824
Samuel Kopecky <samuel.kopecky@heureka.group>
2024-12-23 19:40:17 +0100

    feat(nvim): Change theme

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -1,8 +1,26 @@
+-- return {
+--   'AlexvZyl/nordic.nvim',
+--   lazy = false,
+--   priority = 1000,
+--   config = function()
+--     require('nordic').load()
+--   end,
+-- }
+
 return {
-  'AlexvZyl/nordic.nvim',
+  'HoNamDuong/hybrid.nvim',
   lazy = false,
   priority = 1000,
-  config = function()
-    require('nordic').load()
+  opts = {
+    overrides = function(hl, c)
+      local red = '#ff0000'
+      -- hl.Type = {
+      --   fg = red,
+      -- }
+    end,
+  },
+  config = function(_, opts)
+    require('hybrid').setup(opts)
+    vim.cmd.colorscheme 'hybrid'
   end,
 }
